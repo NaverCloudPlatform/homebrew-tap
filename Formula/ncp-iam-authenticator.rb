7d859dcc26685274c78e248904918d8afc95e972
Mingyum Kim <mingyum.kim@gmail.com>
2023-01-17 14:58:51 +0900

    Init

---
+++ Formula/ncp-iam-authenticator.rb
@@ -0,0 +1,28 @@
+class NcpIamAuthenticator < Formula
+  desc "NaverCloudPlatform Kubernetes Service Authenticator CLI for IAM Authentication"
+  homepage "https://github.com/NaverCloudPlatform/ncp-iam-authenticator"
+  url "https://github.com/NaverCloudPlatform/ncp-iam-authenticator.git",
+      tag:      "v1.0.6",
+      revision: "526e16eeca7f8630a85f14f19701a1bc2ef17450"
+  license "Apache-2.0"
+  head "https://github.com/NaverCloudPlatform/ncp-iam-authenticator.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = ["-s", "-w",
+               "-X github.com/NaverCloudPlatform/ncp-iam-authenticator/cmd.Version=#{version}"]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/ncp-iam-authenticator"
+    prefix.install_metafiles
+  end
+
+  test do
+    output = shell_output("#{bin}/ncp-iam-authenticator version")
+    assert_match %Q("v#{version}"), output
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# NaverCloudPlatform Homebrew Tap
+
+This is the repository for NaverCloudPlatform [Homebrew](http://brew.sh/) packages.
+
+To install a package:
+
+```
+brew tap NaverCloudPlatform/tap
+brew install ncp-iam-authenticator
+```
+
+Available packages:
+
+| Package                   | Description                                      |
+|:--------------------------|:-------------------------------------------------|
+| [ncp-iam-authenticator](https://github.com/NaverCloudPlatform/ncp-iam-authenticator) | The CLI for Kubernetes ServiceIAM authentication |
